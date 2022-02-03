2fb5032faa6e13740989d38129bbb01dbd71026e
iamabhilive <55403463+iamabhilive@users.noreply.github.com>
2022-02-03 21:49:02 +0530

    Create Search in Rotated Sorted Array.cpp

---
+++ Search in Rotated Sorted Array.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int search (vector<int> &nums, int start, int end, int target)
+    {
+	    if (start > end)
+	    {
+		    return -1;
+	    }
+	    int mid = start + (end - start) / 2;
+	
+	    if (nums[mid] == target)
+	    {
+		    return mid;
+	    }
+	
+	    // 1st section sorted or second
+	    if (nums[start] <= nums[mid])
+	    {
+		    // 1st section is sorted
+		    if (nums[start] <= target && target < nums[mid])
+		    {
+			    return search(nums, start, mid - 1, target);
+		    }
+		    else
+		    {
+			    return search(nums, mid + 1, end, target);
+		    }
+	    }
+	    else
+	    {
+		    // 2nd section is sorted
+		    if (nums[mid] < target && target <= nums[end])
+		    {
+			    return search(nums, mid + 1, end, target);
+		    }
+		    else
+		    {
+			    return search(nums, start, mid - 1, target);
+		    }
+	    }
+    }
+    
+    int search(vector<int>& nums, int target) {
+        return search(nums, 0, nums.size() - 1, target);
+    }
+};
